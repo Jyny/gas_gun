465f5e82c216efcfb003791c751b54f580bed33a
jyny <simon54894@gmail.com>
2017-01-09 15:21:37 +0000

    clean code

--- gas_gun.py
+++ gas_gun.py
@@ -1,8 +1,8 @@
 import pygame, sys, os
 from pygame.locals import *
+
 os.environ["SDL_FBDEV"] = "/dev/fb0"
 
-# set up the colors
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 RED   = (255,   0,   0)
@@ -10,14 +10,10 @@ GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 
 pygame.init()
-
-# set up the window
 pygame.mouse.set_visible(False)
-DISPLAYSURF = pygame.display.set_mode([640, 480])
 
-# draw on the surface object
-DISPLAYSURF.fill(WHITE)
+screen = pygame.display.set_mode([640, 480])
+screen.fill(WHITE)
 
-# run the game loop
 while True:
     pygame.display.update()
