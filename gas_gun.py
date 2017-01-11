ef89971cbed25dd03590ee2bf51fea920aa33832
jyny <simon54894@gmail.com>
2017-01-11 17:35:35 +0000

    add flip

--- gas_gun.py
+++ gas_gun.py
@@ -518,5 +518,6 @@ while True:
 	botton_show()
 	#draw_corss(x, y)
 	pygame.display.update()
-	stdout.write ("\r" + ('1 ' if x != -1 and y != -1 else '0 ') + "X:{:5d}".format(x) + " Y:{:5d}".format(y))
-	stdout.flush ()
+	pygame.display.flip()
+	#stdout.write ("\r" + ('1 ' if x != -1 and y != -1 else '0 ') + "X:{:5d}".format(x) + " Y:{:5d}".format(y))
+	#stdout.flush ()
